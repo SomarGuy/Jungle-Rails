d06a96665b0546d9356b89c59c6a63beed64fa53
Somaro <106792137+SomarGuy@users.noreply.github.com>
2023-04-20 18:17:11 -0400

    new file with implemented code for admin authentication

---
+++ app/controllers/concerns/http_basic_auth.rb
@@ -0,0 +1,15 @@
+module HttpBasicAuth
+  extend ActiveSupport::Concern
+
+  included do
+    before_action :authenticate
+  end
+
+  private
+
+  def authenticate
+    authenticate_or_request_with_http_basic('Administration') do |username, password|
+      username == ENV['ADMIN_USERNAME'] && password == ENV['ADMIN_PASSWORD']
+    end
+  end
+end
