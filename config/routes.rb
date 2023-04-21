015abbb95f092a8fac48200e0ae95372c068e163
Somaro <106792137+SomarGuy@users.noreply.github.com>
2023-04-21 19:03:30 -0400

    define routes for user registration, login, and logout

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
   get 'about/index'
   root to: 'products#index'
   get '/about', to: 'about#index'
+  get 'signup', to: 'users#new', as: 'signup'
+  post 'users', to: 'users#create'
+  get 'login', to: 'sessions#new', as: 'login'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy', as: 'logout'
 
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
