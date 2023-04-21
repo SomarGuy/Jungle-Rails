aad677e542be9b25e48ea4478b238add5d4f14c2
Somaro <106792137+SomarGuy@users.noreply.github.com>
2023-04-21 19:07:56 -0400

    implement user registration view, controller actions, and form.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,24 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      session[:user_id] = @user.id
+      flash[:success] = "Welcome to the app!"
+      redirect_to root_path
+    else
+      flash.now[:error] = "There was a problem with your registration."
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+  end
 end
